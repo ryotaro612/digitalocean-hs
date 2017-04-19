f620ee9e4706404f416804c5a772a94f4ca2c17c
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-04-19 13:20:22 +0900

    defining https request

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
+import Network.HTTP.Simple
+import qualified Data.ByteString.Lazy.Char8 as L8
+
+--main :: IO ()
+--main = httpLBS "http://example.com" >>= L8.putStrLn
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+request :: Request
+request = "https://google.com"
+
+-- >>= :: m a -> (a -> m b) -> m b
+--b = httpLBS request
+--aFun = httpLBS "http://google.com" >>= L8.putStrLn
+
+
+
